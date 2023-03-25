467c5d29767e6a96ec4557c51070ff9dbe7ffbce
Willis <favorite.ange@gmail.com>
2023-03-25 20:20:20 +0800

    basic web view

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <Weather msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import Weather from './components/Weather.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Weather,
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Weather.vue
@@ -0,0 +1,6 @@
+<template src="./Weather.html"></template>
+
+<script src="./Weather.js"></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss" src="./Weather.scss"></style>
